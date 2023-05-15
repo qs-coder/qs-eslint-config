1f8f876a2af79f7d697e0e2a933d7f09e1286398
chenpenglei <chenpenglei@huayun.com>
2023-05-15 15:39:24 +0800

    add custom rule for vue

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,8 @@ module.exports = {
   root: true,
   extends: ['airbnb', '@antfu'],
   rules: {
-    curly: ['error', 'all'],
+    'curly': ['error', 'all'],
+    'import/prefer-default-export': 'off',
+    'vue/max-attributes-per-line': ['error', { singleline: { max: 5 }, multiline: { max: 1 } }],
   },
 }
--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const eslintrc = require('./.eslintrc')
 
 module.exports = eslintrc
+
+function main() {
+  return add(4, 5)
+}
+
+function add(a, b) {
+  if (a > 4) {
+    return a + b
+  }
+  return a - b
+}

ae75c32f7ceaa68b3d9c67a494d92d39ab05de89
chenpenglei <chenpenglei@huayun.com>
2023-05-17 14:53:50 +0800

    add no-console

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,10 @@ module.exports = {
   root: true,
   extends: ['airbnb', '@antfu'],
   rules: {
-    'curly': ['error', 'all'],
-    'import/prefer-default-export': 'off',
-    'vue/max-attributes-per-line': ['error', { singleline: { max: 5 }, multiline: { max: 1 } }],
+    'curly': [2, 'all'],
+    'import/prefer-default-export': 0,
+    'vue/max-attributes-per-line': [2, { singleline: { max: 5 }, multiline: { max: 1 } }],
+    'arrow-body-style': [2, 'as-needed'],
+    'no-console': 0,
   },
 }

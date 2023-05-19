bae76dceb0f5a2544b83cf6b1b695241c0ee45a3
chenpenglei <chenpenglei@huayun.com>
2023-05-19 14:32:33 +0800

    remove the test function

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
 const eslintrc = require('./.eslintrc')
 
 module.exports = eslintrc
-
-function main() {
-  return add(4, 5)
-}
-
-function add(a, b) {
-  if (a > 4) {
-    return a + b
-  }
-  return a - b
-}

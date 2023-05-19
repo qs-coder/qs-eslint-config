fc50a991664e8eb3514030307ba0b5846c50ce81
chenpenglei <chenpenglei@huayun.com>
2023-05-19 14:54:20 +0800

    add import/no-extraneous-dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
   rules: {
     'curly': [2, 'all'],
     'import/prefer-default-export': 0,
+    'import/no-extraneous-dependencies': 0,
     'vue/max-attributes-per-line': [2, { singleline: { max: 5 }, multiline: { max: 1 } }],
     'arrow-body-style': [2, 'as-needed'],
     'no-console': 0,
